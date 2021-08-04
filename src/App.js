c4745cf9eec15cec43f5dbbf0836df6818295ade
demonicirfan <hashtag.irfan@gmail.com>
2021-08-04 16:03:56 +0530

    added components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+//import HookCounter from "./components/HookCounter";
+import HookCounter3 from "./components/HookCounter3";
+//import HookCounterTwo from "./components/HookCounterTwo";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <HookCounter3/>
     </div>
   );
 }
